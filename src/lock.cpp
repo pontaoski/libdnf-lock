48ca88eb12b7decdbad6334edb9bbb2e156de42d
Carson Black <uhhadd@gmail.com>
2019-12-11 16:56:07 -0500

    Some improvements?

--- src/lock.cpp
+++ src/lock.cpp
@@ -26,6 +26,20 @@ struct _PluginHandle
     FILE * outStream; // stream to write output
 };
 
+const char* getHookName(PluginHookId id) {
+    if (id == PLUGIN_HOOK_ID_CONTEXT_PRE_CONF) {
+        return "Pre-Conf";
+    } else if (id == PLUGIN_HOOK_ID_CONTEXT_CONF) {
+        return "Conf";
+    } else if (id == PLUGIN_HOOK_ID_CONTEXT_PRE_TRANSACTION) {
+        return "Pre-Trans";
+    } else if (id == PLUGIN_HOOK_ID_CONTEXT_TRANSACTION) {
+        return "Trans";
+    } else if (id == PLUGIN_HOOK_ID_CONTEXT_PRE_REPOS_RELOAD) {
+        return "Pre-Reload";
+    }
+}
+
 PluginHandle* pluginInitHandle(int version, PluginMode mode, DnfPluginInitData* initData) {
     PluginHandle* handle = NULL;
     FILE* outStream = fopen("/tmp/libdnflock.log", "a");
@@ -69,8 +83,10 @@ int pluginHook(PluginHandle* handle, PluginHookId id, DnfPluginHookData* hookDat
     if (!handle)
         return 1;
     fprintf(handle->outStream, "===\t< %s | %s> [ID: %i]\tEnter\n", info.name, __func__, id);
-    if (id == PLUGIN_HOOK_ID_CONTEXT_PRE_REPOS_RELOAD) {
+    fflush(handle->outStream);
+    if (id == PLUGIN_HOOK_ID_CONTEXT_TRANSACTION) {
         fprintf(handle->outStream, "Pre-Transaction Hook!\n\n");
+        fflush(handle->outStream);
         DnfSack* sack = dnf_context_get_sack(handle->context);
         std::ifstream in("/etc/dnf/plugins/versionlock.list");
         std::string str;
@@ -80,23 +96,40 @@ int pluginHook(PluginHandle* handle, PluginHookId id, DnfPluginHookData* hookDat
             if(str.size() > 0) {
                 if (strncmp(str.c_str(), std::string("#").c_str(), std::string("#").size())) {
                     fprintf(handle->outStream, "lock %s!\n", str.c_str());
+                    fflush(handle->outStream);
                     strVect.push_back(str);
                 } else {
                     fprintf(handle->outStream, "\tnot %s!\n", str.c_str());
+                    fflush(handle->outStream);
                 }
             }
         }
         in.close();
 
         for (int i = 0; i < strVect.size(); i++) {
+            fprintf(handle->outStream, "Querying for %s!\n", strVect[i].c_str());
+            fflush(handle->outStream);
             HyQuery query = hy_query_create(sack);
-            hy_query_filter(query, HY_PKG_NAME, HY_GLOB, strVect[i].c_str());
-            const Map* set = hy_query_get_result(query);
-            Map* pkset = const_cast<Map*&>(set);
-            dnf_sack_add_excludes(sack, dnf_packageset_from_bitmap(sack, pkset));
+            hy_query_is_empty(query);
+            int result = hy_query_filter(query, HY_PKG_NAME, HY_GLOB, strVect[i].c_str());
+            fprintf(handle->outStream, "Query result is %i!\n", result);
+            fflush(handle->outStream);
+            if (result == DNF_ERROR_BAD_QUERY) {
+                fprintf(handle->outStream, "Bad query!\n");
+                fflush(handle->outStream);
+            } else {
+                if (!hy_query_is_empty(query)) {
+                    DnfPackageSet* pkgset = hy_query_run_set(query);
+                    dnf_sack_add_excludes(sack, pkgset);
+                } else {
+                    fprintf(handle->outStream, "Lock query is empty!");
+                    fflush(handle->outStream);
+                }
+            }
         }
     } else {
-        fprintf(handle->outStream, "Not handling hook %i, because it's not %i!\n", id, PLUGIN_HOOK_ID_CONTEXT_PRE_REPOS_RELOAD);
+        fprintf(handle->outStream, "Not handling hook %s, because it's not %s!\n", getHookName(id), getHookName(PLUGIN_HOOK_ID_CONTEXT_TRANSACTION));
+        fflush(handle->outStream);
     }
     fprintf(handle->outStream, "===\t< %s | %s> [ID: %i]\tExit\n\n\n", info.name, __func__, id);
     fflush(handle->outStream);
